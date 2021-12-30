bfb7a465fb811eecbe4f9b2e51d701933cbf6da1
Hexawolf <vyamkovyi@elastio.com>
2021-12-30 06:15:51 +0200

    Update licensing
    All code-containing files (.rs) were updated to include a proper copyright header. This also changes some existing headers by including "Leafish Developers" attribution and - most importantly - changes the template itself to a widely accepted form for MIT/Apache-2.0. Previously headers used Apache-2.0 statement exclusively as if it was the only license.
    
    Improve license specification in README.md to clarify that any of two licenses can be selected by end-users, but any contributions to the project will necessarily go under both licenses. The major point of such dual-licensing is exactly making contributions go through Apache-2.0 "Grant of Patent License" clause which spares project from possibility of patent abuse by bad actors.

--- src/render/clouds.rs
+++ src/render/clouds.rs
@@ -1,4 +1,10 @@
-use std::sync::Arc;
+// Copyright 2021-2022 Leafish Developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
 use super::glsl;
 use crate::gl;
@@ -6,6 +12,7 @@ use byteorder::{NativeEndian, WriteBytesExt};
 use cgmath::{Matrix4, Point3};
 use log::error;
 use parking_lot::RwLock;
+use std::sync::Arc;
 
 pub struct Clouds {
     program: gl::Program,
