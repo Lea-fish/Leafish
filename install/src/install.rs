23f9ad5231a9c4829f896c20142d75818304b856
threadexception <hannes.gaumann@outlook.de>
2024-02-25 14:09:03 +0100

    Give feedback in installer

--- install/src/install.rs
+++ install/src/install.rs
@@ -84,24 +84,30 @@ pub fn setup_launcher_wrapper(prefix: &str) -> anyhow::Result<bool> {
     let json_path = format!("{}{}", prefix, DESC_JSON_PATH);
     let jar_path = format!("{}{}", prefix, JAR_PATH);
     if Path::new(&json_path).exists() && Path::new(&jar_path).exists() {
+        println!("Leafish is already installed");
         return Ok(false);
     }
     // cleanup old files
     if Path::new(&json_path).exists() {
+        println!("Removing old json...");
         fs::remove_file(&json_path)?;
     }
     if Path::new(&jar_path).exists() {
+        println!("Removing old jar...");
         fs::remove_file(&jar_path)?;
     }
 
-    println!("dir: {}", format!("{}{}", prefix, DIR_PATH));
-
-    // create directory if necessary
-    fs::create_dir(format!("{}{}", prefix, DIR_PATH))?;
+    println!("Removing old json...");
+    let dir_path = format!("{}{}", prefix, DIR_PATH);
+    if !Path::new(&dir_path).exists() {
+        println!("Creating version directory...");
+        // create directory if necessary
+        fs::create_dir(&dir_path)?;
+    }
 
     // write files
+    println!("Creating json...");
     let mut json = File::create_new(&json_path)?;
-    let mut jar = File::create_new(&jar_path)?;
     json.write_all(serde_json::to_string_pretty(&Description {
         id: "Leafish".to_string(),
         inherits_from: "1.8.9".to_string(), // FIXME: is this a good default?
@@ -110,20 +116,25 @@ pub fn setup_launcher_wrapper(prefix: &str) -> anyhow::Result<bool> {
         ty: "release".to_string(),
         libraries: vec![Library { name: "leafish:Leafish:Jar".to_string() }], // we need nobody, but ourselves ;)
         main_class: "de.leafish.Main".to_string(),
-        minecraft_arguments: "--username ${auth_player_name} --version ${version_name} --gameDir ${game_directory} --assetsDir ${assets_root} --assetIndex ${assets_index_name} --uuid ${auth_uuid} --accessToken ${auth_access_token} --userProperties ${user_properties} --userType ${user_type}".to_string(),
+        minecraft_arguments: "--username ${auth_player_name} --gameDir ${game_directory} --assetsDir ${assets_root} --assetIndex ${assets_index_name} --uuid ${auth_uuid} --accessToken ${auth_access_token} --userProperties ${user_properties} --userType ${user_type}".to_string(),
     })?.as_bytes())?;
 
     // FIXME: download the latest jar from github
     let raw_jar = include_bytes!("../resources/wrapper.jar");
 
+    println!("Copying version jar...");
+    let mut jar = File::create_new(&jar_path)?;
     jar.write_all(raw_jar)?;
 
     let lib_dir = format!("{}{}", prefix, LIBRARY_DIR_PATH);
     if !Path::new(&lib_dir).exists() {
+        println!("Copying library...");
         fs::create_dir_all(&lib_dir)?;
         let mut file = File::create_new(format!("{}{}", prefix, LIBRARY_PATH))?;
         file.write_all(raw_jar)?;
     }
+
+    println!("Installation successful!");
 
     /*let profiles_path = format!("{}{}", prefix, PROFILES_JSON_PATH);
     if !Path::new(&profiles_path).exists() {
