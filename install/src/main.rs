374f5c0a809b9e31951b7125e78e613a9a66b855
threadexception <hannes.gaumann@outlook.de>
2024-03-02 02:33:43 +0100

    Initial commit

--- install/src/main.rs
+++ install/src/main.rs
@@ -1,14 +1,17 @@
-use install::setup_launcher_wrapper;
+use install::{mojang, prism};
 
 mod install;
 
 fn main() {
-    let dir = mc_dir();
-    setup_launcher_wrapper(&dir).unwrap();
+    let mojang_dir = mojang_dir();
+    mojang::setup(&mojang_dir).unwrap();
+    for dir in prism_dirs() {
+        prism::setup(&dir).unwrap();
+    }
 }
 
 #[cfg(any(target_os = "windows", target_os = "macos"))]
-fn mc_dir() -> String {
+fn mojang_dir() -> String {
     platform_dirs::AppDirs::new(Some(".minecraft"), false)
         .unwrap()
         .config_dir
@@ -18,7 +21,7 @@ fn mc_dir() -> String {
 }
 
 #[cfg(target_os = "linux")]
-fn mc_dir() -> String {
+fn mojang_dir() -> String {
     let state = platform_dirs::AppDirs::new(None, false).unwrap();
     let full = state.config_dir.to_str().unwrap();
     format!(
@@ -26,4 +29,36 @@ fn mc_dir() -> String {
         &full[0..(full.len() - ".config".len())],
         ".minecraft"
     )
+}
+
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+fn prism_dirs() -> Vec<String> {
+    vec![platform_dirs::AppDirs::new(Some("PrismLauncher"), false)
+        .unwrap()
+        .config_dir
+        .to_str()
+        .unwrap()
+        .to_string()]
+}
+
+#[cfg(target_os = "linux")]
+fn prism_dirs() -> Vec<String> {
+    let flatpak = {
+        let state = platform_dirs::AppDirs::new(None, false).unwrap();
+        let full = state.config_dir.to_str().unwrap();
+        format!(
+            "{}{}",
+            &full[0..(full.len() - ".config".len())],
+            ".var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher"
+        )
+    };
+    vec![
+        platform_dirs::AppDirs::new(None, false)
+            .unwrap()
+            .data_dir
+            .to_str()
+            .unwrap()
+            .to_string(),
+        flatpak,
+    ]
 }
